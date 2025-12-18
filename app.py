b0fe1d9653bddb13234713e5c663c93276a61708
RitikaPC <89989852+RitikaPC@users.noreply.github.com>
2025-12-18 13:39:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,5 +151,7 @@ def run_workflow():
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 200
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
