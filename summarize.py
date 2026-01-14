bb9eaf04475511ebbec2ac1874de5a205e1eaed9
t0349e1 <ritika.palchaudhuri@stellantis.com>
2026-01-14 11:42:38 +0530

    Add comprehensive anchor navigation for Confluence compatibility
    - Added JavaScript scrollToComponent function for manual navigation control
    - Added multiple anchor formats: id, name attribute, and <a name> tags
    - Uses both standard anchor links (#component) and JavaScript fallback
    - Structure: <div id='component' name='component'><a name='component'></a><h2>...</h2>
    - Should work across different environments including Confluence
    - Covers all possible anchor navigation methods for maximum compatibility

--- summarize.py
+++ summarize.py
@@ -431,7 +431,8 @@ def generate_component_toc():
     for component in ["APIM", "EAH", "DOCG", "VDR", "PATRIC-SSDP", "RCZ", "SYNAPSE", "REFTEL", "CALVA", "REFSER2", "SERING"]:
         if component in pv and pv[component]:
             anchor_id = component_anchors[component]
-            component_links.append(f'<li><a href="#{anchor_id}" style="color:#0066CC;text-decoration:none;">{component}</a></li>')
+            # Use both anchor link and JavaScript fallback for better compatibility
+            component_links.append(f'<li><a href="#{anchor_id}" onclick="scrollToComponent(\'{anchor_id}\'); return true;" style="color:#0066CC;text-decoration:none;">{component}</a></li>')
     
     return '\n'.join(component_links)
 
@@ -520,7 +521,7 @@ for ver in sorted(pv["APIM"].keys(), key=vtuple):
         extra="<a href='https://pages.github.psa-cloud.com/mph00/cloud-api-capabilities/#/changelog' target='_blank'>Swagger Changelog</a>"
     )
 if apim_html.strip():
-    section_html += "<div id=\"apim\"><h2>APIM</h2>\n" + apim_html + "</div>"
+    section_html += "<div id=\"apim\" name=\"apim\"><a name=\"apim\"></a><h2>APIM</h2>\n" + apim_html + "</div>"
 
 # EAH
 eah_html = ""
@@ -535,7 +536,7 @@ for ver in sorted(pv["EAH"].keys(), key=vtuple):
         extra="<a href='https://pages.github.psa-cloud.com/mph00/cloud-api-capabilities/#/changelog' target='_blank'>Swagger Changelog</a>"
     )
 if eah_html.strip():
-    section_html += "<div id=\"eah\"><h2>EAH</h2>\n" + eah_html + "</div>"
+    section_html += "<div id=\"eah\" name=\"eah\"><a name=\"eah\"></a><h2>EAH</h2>\n" + eah_html + "</div>"
 
 # DOCG
 docg_html = ""
@@ -551,7 +552,7 @@ for ver in sorted(pv["DOCG"].keys(), key=vtuple):
         extra=extra
     )
 if docg_html.strip():
-    section_html += "<div id=\"docg\"><h2>DOCG</h2>\n" + docg_html + "</div>"
+    section_html += "<div id=\"docg\" name=\"docg\"><a name=\"docg\"></a><h2>DOCG</h2>\n" + docg_html + "</div>"
 
 # VDR
 vdr_html = ""
@@ -567,7 +568,7 @@ for ver in sorted(pv.get("VDR", {}).keys(), key=vtuple):
         extra=extra
     )
 if vdr_html.strip():
-    section_html += "<div id=\"vdr\"><h2>VDR</h2>\n" + vdr_html + "</div>"
+    section_html += "<div id=\"vdr\" name=\"vdr\"><a name=\"vdr\"></a><h2>VDR</h2>\n" + vdr_html + "</div>"
 
 # PATRIC-SSDP
 patric_html = ""
@@ -583,7 +584,7 @@ for ver in sorted(pv.get("PATRIC-SSDP", {}).keys(), key=vtuple):
         extra=extra
     )
 if patric_html.strip():
-    section_html += "<div id=\"patric-ssdp\"><h2>PATRIC-SSDP</h2>\n" + patric_html + "</div>"
+    section_html += "<div id=\"patric-ssdp\" name=\"patric-ssdp\"><a name=\"patric-ssdp\"></a><h2>PATRIC-SSDP</h2>\n" + patric_html + "</div>"
 
 # RCZ
 rcz_html = ""
@@ -600,7 +601,7 @@ for ver in sorted(pv.get("RCZ", {}).keys(), key=vtuple):
     )
 
 if rcz_html.strip():
-    section_html += "<div id=\"rcz\"><h2>RCZ</h2>\n" + rcz_html + "</div>"
+    section_html += "<div id=\"rcz\" name=\"rcz\"><a name=\"rcz\"></a><h2>RCZ</h2>\n" + rcz_html + "</div>"
 
 # SYNAPSE
 synapse_html = ""
@@ -617,7 +618,7 @@ for ver in sorted(pv.get("SYNAPSE", {}).keys(), key=vtuple):
     )
 
 if synapse_html.strip():
-    section_html += "<div id=\"synapse\"><h2>SYNAPSE</h2>\n" + synapse_html + "</div>"
+    section_html += "<div id=\"synapse\" name=\"synapse\"><a name=\"synapse\"></a><h2>SYNAPSE</h2>\n" + synapse_html + "</div>"
 
 # REFTEL
 reftel_html = ""
@@ -634,7 +635,7 @@ for ver in sorted(pv.get("REFTEL", {}).keys(), key=vtuple):
     )
 
 if reftel_html.strip():
-    section_html += "<div id=\"reftel\"><h2>REFTEL</h2>\n" + reftel_html + "</div>"
+    section_html += "<div id=\"reftel\" name=\"reftel\"><a name=\"reftel\"></a><h2>REFTEL</h2>\n" + reftel_html + "</div>"
 
 # CALVA
 calva_html = ""
@@ -651,7 +652,7 @@ for ver in sorted(pv.get("CALVA", {}).keys(), key=vtuple):
     )
 
 if calva_html.strip():
-    section_html += "<div id=\"calva\"><h2>CALVA</h2>\n" + calva_html + "</div>"
+    section_html += "<div id=\"calva\" name=\"calva\"><a name=\"calva\"></a><h2>CALVA</h2>\n" + calva_html + "</div>"
 
 # REFSER2
 refser2_html = ""
@@ -668,7 +669,7 @@ for ver in sorted(pv.get("REFSER2", {}).keys(), key=vtuple):
     )
 
 if refser2_html.strip():
-    section_html += "<div id=\"refser2\"><h2>REFSER2</h2>\n" + refser2_html + "</div>"
+    section_html += "<div id=\"refser2\" name=\"refser2\"><a name=\"refser2\"></a><h2>REFSER2</h2>\n" + refser2_html + "</div>"
 
 # SERING
 sering_html = ""
@@ -685,13 +686,24 @@ for ver in sorted(pv.get("SERING", {}).keys(), key=vtuple):
     )
 
 if sering_html.strip():
-    section_html += "<div id=\"sering\"><h2>SERING</h2>\n" + sering_html + "</div>"
+    section_html += "<div id=\"sering\" name=\"sering\"><a name=\"sering\"></a><h2>SERING</h2>\n" + sering_html + "</div>"
 
 
 linked_html = build_linked_table(blocks)
 
 html = f"""
 <h1 style="color:#0747A6;">SSDP Release Notes Week {week_display}</h1>
+
+<script>
+function scrollToComponent(componentId) {{
+    var element = document.getElementById(componentId);
+    if (element) {{
+        element.scrollIntoView({{behavior: 'smooth'}});
+        // Update URL hash for better navigation
+        window.location.hash = '#' + componentId;
+    }}
+}}
+</script>
 
 {release_summary_html}
 
