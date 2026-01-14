663445871a46604ea2e0f59dc980e2d51d5a8ed4
t0349e1 <ritika.palchaudhuri@stellantis.com>
2026-01-14 11:34:43 +0530

    Fix component navigation by wrapping sections in div containers
    - Wrapped each component section in div with anchor ID instead of just h2 header
    - Structure changed from: <h2 id='component'>Title</h2> + content
    - To: <div id='component'><h2>Title</h2> + content + </div>
    - This creates proper anchor targets that browsers can navigate to
    - Should resolve table of contents links not scrolling to components

--- summarize.py
+++ summarize.py
@@ -520,7 +520,7 @@ for ver in sorted(pv["APIM"].keys(), key=vtuple):
         extra="<a href='https://pages.github.psa-cloud.com/mph00/cloud-api-capabilities/#/changelog' target='_blank'>Swagger Changelog</a>"
     )
 if apim_html.strip():
-    section_html += "<h2 id=\"apim\">APIM</h2>\n" + apim_html
+    section_html += "<div id=\"apim\"><h2>APIM</h2>\n" + apim_html + "</div>"
 
 # EAH
 eah_html = ""
@@ -535,7 +535,7 @@ for ver in sorted(pv["EAH"].keys(), key=vtuple):
         extra="<a href='https://pages.github.psa-cloud.com/mph00/cloud-api-capabilities/#/changelog' target='_blank'>Swagger Changelog</a>"
     )
 if eah_html.strip():
-    section_html += "<h2 id=\"eah\">EAH</h2>\n" + eah_html
+    section_html += "<div id=\"eah\"><h2>EAH</h2>\n" + eah_html + "</div>"
 
 # DOCG
 docg_html = ""
@@ -551,7 +551,7 @@ for ver in sorted(pv["DOCG"].keys(), key=vtuple):
         extra=extra
     )
 if docg_html.strip():
-    section_html += "<h2 id=\"docg\">DOCG</h2>\n" + docg_html
+    section_html += "<div id=\"docg\"><h2>DOCG</h2>\n" + docg_html + "</div>"
 
 # VDR
 vdr_html = ""
@@ -567,7 +567,7 @@ for ver in sorted(pv.get("VDR", {}).keys(), key=vtuple):
         extra=extra
     )
 if vdr_html.strip():
-    section_html += "<h2 id=\"vdr\">VDR</h2>\n" + vdr_html
+    section_html += "<div id=\"vdr\"><h2>VDR</h2>\n" + vdr_html + "</div>"
 
 # PATRIC-SSDP
 patric_html = ""
@@ -583,7 +583,7 @@ for ver in sorted(pv.get("PATRIC-SSDP", {}).keys(), key=vtuple):
         extra=extra
     )
 if patric_html.strip():
-    section_html += "<h2 id=\"patric-ssdp\">PATRIC-SSDP</h2>\n" + patric_html
+    section_html += "<div id=\"patric-ssdp\"><h2>PATRIC-SSDP</h2>\n" + patric_html + "</div>"
 
 # RCZ
 rcz_html = ""
@@ -600,7 +600,7 @@ for ver in sorted(pv.get("RCZ", {}).keys(), key=vtuple):
     )
 
 if rcz_html.strip():
-    section_html += "<h2 id=\"rcz\">RCZ</h2>\n" + rcz_html
+    section_html += "<div id=\"rcz\"><h2>RCZ</h2>\n" + rcz_html + "</div>"
 
 # SYNAPSE
 synapse_html = ""
@@ -617,7 +617,7 @@ for ver in sorted(pv.get("SYNAPSE", {}).keys(), key=vtuple):
     )
 
 if synapse_html.strip():
-    section_html += "<h2 id=\"synapse\">SYNAPSE</h2>\n" + synapse_html
+    section_html += "<div id=\"synapse\"><h2>SYNAPSE</h2>\n" + synapse_html + "</div>"
 
 # REFTEL
 reftel_html = ""
@@ -634,7 +634,7 @@ for ver in sorted(pv.get("REFTEL", {}).keys(), key=vtuple):
     )
 
 if reftel_html.strip():
-    section_html += "<h2 id=\"reftel\">REFTEL</h2>\n" + reftel_html
+    section_html += "<div id=\"reftel\"><h2>REFTEL</h2>\n" + reftel_html + "</div>"
 
 # CALVA
 calva_html = ""
@@ -651,7 +651,7 @@ for ver in sorted(pv.get("CALVA", {}).keys(), key=vtuple):
     )
 
 if calva_html.strip():
-    section_html += "<h2 id=\"calva\">CALVA</h2>\n" + calva_html
+    section_html += "<div id=\"calva\"><h2>CALVA</h2>\n" + calva_html + "</div>"
 
 # REFSER2
 refser2_html = ""
@@ -668,7 +668,7 @@ for ver in sorted(pv.get("REFSER2", {}).keys(), key=vtuple):
     )
 
 if refser2_html.strip():
-    section_html += "<h2 id=\"refser2\">REFSER2</h2>\n" + refser2_html
+    section_html += "<div id=\"refser2\"><h2>REFSER2</h2>\n" + refser2_html + "</div>"
 
 # SERING
 sering_html = ""
@@ -685,7 +685,7 @@ for ver in sorted(pv.get("SERING", {}).keys(), key=vtuple):
     )
 
 if sering_html.strip():
-    section_html += "<h2 id=\"sering\">SERING</h2>\n" + sering_html
+    section_html += "<div id=\"sering\"><h2>SERING</h2>\n" + sering_html + "</div>"
 
 
 linked_html = build_linked_table(blocks)
